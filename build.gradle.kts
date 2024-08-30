91f2bec31c6f4acdaa04583a04a079ef26580313
Nat <nat@manleyfamily.co.uk>
2024-08-30 11:44:20 +0100

    remove juinitXml output location

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
 
 tasks.test {
     reports {
-        junitXml.outputLocation = layout.buildDirectory.dir("test-junit-xml")
         junitXml.required.set(true)
         html.required.set(false)
     }
