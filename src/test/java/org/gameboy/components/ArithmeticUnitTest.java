44aa2093315d471f243f23fef32c7a9b7fdfd973
Nat <nat@manleyfamily.co.uk>
2024-10-08 17:05:19 +0100

    Add 'and' operation to ArithmeticUnit

--- src/test/java/org/gameboy/components/ArithmeticUnitTest.java
+++ src/test/java/org/gameboy/components/ArithmeticUnitTest.java
@@ -1,20 +1,23 @@
 package org.gameboy.components;
 
 import org.gameboy.Flag;
+import org.gameboy.GameboyAssertions;
 import org.gameboy.components.ArithmeticUnit.ArithmeticResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Hashtable;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.gameboy.Flag.*;
 import static org.gameboy.FlagValue.setFlag;
 import static org.gameboy.FlagValue.unsetFlag;
-import static org.gameboy.Flag.*;
 import static org.gameboy.utils.BitUtilities.uint;
 
 class ArithmeticUnitTest {
@@ -37,14 +40,14 @@ class ArithmeticUnitTest {
 
     static Stream<Arguments> getIncValues() {
         return Stream.of(
-                Arguments.of((byte) 0x00, new Entry[] {unsetFlag(Z), unsetFlag(N), unsetFlag(H)}),
-                Arguments.of((byte) 0x0f, new Entry[] {unsetFlag(Z), unsetFlag(N), setFlag(H)}),
-                Arguments.of((byte) 0xff, new Entry[] {setFlag(Z), unsetFlag(N), setFlag(H)})
+                Arguments.of((byte) 0x00, new Entry[]{unsetFlag(Z), unsetFlag(N), unsetFlag(H)}),
+                Arguments.of((byte) 0x0f, new Entry[]{unsetFlag(Z), unsetFlag(N), setFlag(H)}),
+                Arguments.of((byte) 0xff, new Entry[]{setFlag(Z), unsetFlag(N), setFlag(H)})
         );
     }
 
     @SafeVarargs
-    @ParameterizedTest(name="{0}")
+    @ParameterizedTest(name = "{0}")
     @MethodSource("getIncValues")
     final void givenByte_whenInc_thenFlagsCorrect(byte value, Entry<Flag, Boolean>... expectedFlags) {
         ArithmeticResult result = alu.inc(value);
@@ -64,16 +67,16 @@ class ArithmeticUnitTest {
 
     static Stream<Arguments> getDecValues() {
         return Stream.of(
-                Arguments.of((byte) 0x00, new Entry[] {unsetFlag(Z), setFlag(N), setFlag(H)}),
-                Arguments.of((byte) 0x0f, new Entry[] {unsetFlag(Z), setFlag(N), unsetFlag(H)}),
-                Arguments.of((byte) 0xff, new Entry[] {unsetFlag(Z), setFlag(N), unsetFlag(H)}),
-                Arguments.of((byte) 0xf0, new Entry[] {unsetFlag(Z), setFlag(N), setFlag(H)}),
-                Arguments.of((byte) 0x01, new Entry[] {setFlag(Z), setFlag(N), unsetFlag(H)})
+                Arguments.of((byte) 0x00, new Entry[]{unsetFlag(Z), setFlag(N), setFlag(H)}),
+                Arguments.of((byte) 0x0f, new Entry[]{unsetFlag(Z), setFlag(N), unsetFlag(H)}),
+                Arguments.of((byte) 0xff, new Entry[]{unsetFlag(Z), setFlag(N), unsetFlag(H)}),
+                Arguments.of((byte) 0xf0, new Entry[]{unsetFlag(Z), setFlag(N), setFlag(H)}),
+                Arguments.of((byte) 0x01, new Entry[]{setFlag(Z), setFlag(N), unsetFlag(H)})
         );
     }
 
     @SafeVarargs
-    @ParameterizedTest(name="{0}")
+    @ParameterizedTest(name = "{0}")
     @MethodSource("getDecValues")
     final void givenByte_whenDec_thenFlagsCorrect(byte value, Entry<Flag, Boolean>... expectedFlags) {
         ArithmeticResult result = alu.dec(value);
@@ -95,7 +98,7 @@ class ArithmeticUnitTest {
     @ParameterizedTest
     @MethodSource("getBasicOperationValues")
     void givenTwoBytes_whenAdd_thenResultCorrect(byte a, byte b) {
-        byte expectedResult = (byte) (a+b);
+        byte expectedResult = (byte) (a + b);
 
         ArithmeticResult result = alu.add(a, b);
 
@@ -114,17 +117,17 @@ class ArithmeticUnitTest {
 
     static Stream<Arguments> getAddValues() {
         return Stream.of(
-                Arguments.of((byte) 0x00, (byte) 0x00, new Entry[] {setFlag(Z), unsetFlag(N), unsetFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0x0f, (byte) 0x0f, new Entry[] {unsetFlag(Z), unsetFlag(N), setFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0xff, (byte) 0xff, new Entry[] {unsetFlag(Z), unsetFlag(N), setFlag(H), setFlag(C)}),
-                Arguments.of((byte) 0xa5, (byte) 0x67, new Entry[] {unsetFlag(Z), unsetFlag(N), unsetFlag(H), setFlag(C)}),
-                Arguments.of((byte) 0xc1, (byte) 0x11, new Entry[] {unsetFlag(Z), unsetFlag(N), unsetFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0xa3, (byte) 0x5d, new Entry[] {setFlag(Z), unsetFlag(N), setFlag(H), setFlag(C)})
+                Arguments.of((byte) 0x00, (byte) 0x00, new Entry[]{setFlag(Z), unsetFlag(N), unsetFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0x0f, (byte) 0x0f, new Entry[]{unsetFlag(Z), unsetFlag(N), setFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0xff, (byte) 0xff, new Entry[]{unsetFlag(Z), unsetFlag(N), setFlag(H), setFlag(C)}),
+                Arguments.of((byte) 0xa5, (byte) 0x67, new Entry[]{unsetFlag(Z), unsetFlag(N), unsetFlag(H), setFlag(C)}),
+                Arguments.of((byte) 0xc1, (byte) 0x11, new Entry[]{unsetFlag(Z), unsetFlag(N), unsetFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0xa3, (byte) 0x5d, new Entry[]{setFlag(Z), unsetFlag(N), setFlag(H), setFlag(C)})
         );
     }
 
     @SafeVarargs
-    @ParameterizedTest(name="{0}+{1}")
+    @ParameterizedTest(name = "{0}+{1}")
     @MethodSource("getAddValues")
     final void givenTwoBytes_whenAdd_thenFlagsCorrect(byte a, byte b, Entry<Flag, Boolean>... expectedFlags) {
         ArithmeticResult result = alu.add(a, b);
@@ -133,25 +136,48 @@ class ArithmeticUnitTest {
     }
 
 
-
     static Stream<Arguments> getSubValues() {
         return Stream.of(
-                Arguments.of((byte) 0x00, (byte) 0x00, new Entry[] {setFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0x0f, (byte) 0x0f, new Entry[] {setFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0xff, (byte) 0xff, new Entry[] {setFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0xa5, (byte) 0x67, new Entry[] {unsetFlag(Z), setFlag(N), setFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0xc1, (byte) 0x11, new Entry[] {unsetFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
-                Arguments.of((byte) 0x30, (byte) 0xa0, new Entry[] {unsetFlag(Z), setFlag(N), unsetFlag(H), setFlag(C)}),
-                Arguments.of((byte) 0x32, (byte) 0xab, new Entry[] {unsetFlag(Z), setFlag(N), setFlag(H), setFlag(C)})
+                Arguments.of((byte) 0x00, (byte) 0x00, new Entry[]{setFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0x0f, (byte) 0x0f, new Entry[]{setFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0xff, (byte) 0xff, new Entry[]{setFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0xa5, (byte) 0x67, new Entry[]{unsetFlag(Z), setFlag(N), setFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0xc1, (byte) 0x11, new Entry[]{unsetFlag(Z), setFlag(N), unsetFlag(H), unsetFlag(C)}),
+                Arguments.of((byte) 0x30, (byte) 0xa0, new Entry[]{unsetFlag(Z), setFlag(N), unsetFlag(H), setFlag(C)}),
+                Arguments.of((byte) 0x32, (byte) 0xab, new Entry[]{unsetFlag(Z), setFlag(N), setFlag(H), setFlag(C)})
         );
     }
 
     @SafeVarargs
-    @ParameterizedTest(name="{0}-{1}")
+    @ParameterizedTest(name = "{0}-{1}")
     @MethodSource("getSubValues")
     final void givenTwoBytes_whenSub_thenFlagsCorrect(byte a, byte b, Entry<Flag, Boolean>... expectedFlags) {
         ArithmeticResult result = alu.sub(a, b);
 
         assertThat(result.flagChanges()).containsOnly(expectedFlags);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0x00, 0x73", "0xff, 0x43", "0xf1, 0x1f", "0x23, 0x23"})
+    void givenTwoBytes_whenAnd_thenResultCorrect(int a, int b) {
+        ArithmeticResult result = alu.and((byte) a, (byte) b);
+
+        int expected = a & b;
+
+        assertThat(result.result()).isEqualTo((byte) expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0x00, 0x73", "0xff, 0x43", "0xf1, 0x1f", "0x23, 0x23"})
+    void givenTwoBytes_whenAnd_thenFlagsCorrect(int a, int b) {
+        ArithmeticResult result = alu.and((byte) a, (byte) b);
+
+        Hashtable<Flag, Boolean> expectedFlags = new ArithmeticUnit.FlagChangesetBuilder()
+                .with(Flag.Z, a == 0 || b == 0)
+                .with(Flag.H, true)
+                .with(Flag.C, false)
+                .with(Flag.N, false)
+                .build();
+        GameboyAssertions.assertFlagsMatch(expectedFlags, result.flagChanges());
     }
 }
