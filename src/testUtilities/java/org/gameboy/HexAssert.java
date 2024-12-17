bd757ac432055acd74e2161397cbc56a49079608
Nat Manley <nat@manleyfamily.co.uk>
2024-12-17 07:53:09 +0000

    Add pop and push operations to ControlFlow.

---
+++ src/testUtilities/java/org/gameboy/HexAssert.java
@@ -0,0 +1,20 @@
+package org.gameboy;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ObjectAssert;
+
+public class HexAssert<T extends Number> {
+    private final T actual;
+    private final ObjectAssert<T> assertion;
+
+    public HexAssert(T actual) {
+        this.actual = actual;
+        assertion = Assertions.assertThat(actual);
+    }
+
+    public void isEqualTo(Number expected) {
+        assertion
+                .withFailMessage("expecting:\n0x%x\n to be equal to:\n%x\nbut was not.".formatted(actual, expected))
+                .isEqualTo(expected);
+    }
+}
