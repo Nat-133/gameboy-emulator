04580c0bed0b814cb14f87cf08b20ca05d6e1fe8
Nat Manley <nat@manleyfamily.co.uk>
2025-01-06 07:55:03 +0000

    Remove legacy constructor from Return instruction.

--- src/main/java/org/gameboy/instructions/Return.java
+++ src/main/java/org/gameboy/instructions/Return.java
@@ -2,17 +2,12 @@ package org.gameboy.instructions;
 
 import org.gameboy.components.CpuStructure;
 import org.gameboy.instructions.common.ControlFlow;
-import org.gameboy.instructions.targets.Condition;
 
 public class Return implements Instruction {
     private Return() {
     }
 
     public static Return ret() {
-        return new Return();
-    }
-
-    public static Return ret_cc(Condition cc) {
         return new Return();
     }
 
