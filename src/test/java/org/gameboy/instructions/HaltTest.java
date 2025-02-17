d7b9ea274c71d3444eab9f193abc4edf74d911e7
Nat Manley <nat@manleyfamily.co.uk>
2025-02-17 08:49:50 +0000

    Complete Halt instruction.

--- src/test/java/org/gameboy/instructions/HaltTest.java
+++ src/test/java/org/gameboy/instructions/HaltTest.java
@@ -1,13 +1,17 @@
 package org.gameboy.instructions;
 
+import org.gameboy.Cpu;
 import org.gameboy.CpuStructureBuilder;
 import org.gameboy.components.Clock;
 import org.gameboy.components.CpuStructure;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeoutException;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.gameboy.MemoryMapConstants.IE_ADDRESS;
-import static org.gameboy.MemoryMapConstants.IF_ADDRESS;
+import static org.gameboy.GameboyAssertions.assertThatHex;
+import static org.gameboy.MemoryMapConstants.*;
+import static org.gameboy.TestUtils.waitFor;
 
 class HaltTest {
     @Test
@@ -16,6 +20,7 @@ class HaltTest {
         CpuStructure cpuStructure = new CpuStructureBuilder()
                 .withMemory(IF_ADDRESS, 0x0f)
                 .withMemory(IE_ADDRESS, 0x0f)
+                .withIME(true)
                 .withClock(clock)
                 .build();
 
@@ -24,6 +29,69 @@ class HaltTest {
         assertThat(clock.isStopped).isFalse();
         assertThat(clock.hasStopped).isTrue();
         assertThat(clock.hasStarted).isTrue();
+    }
+
+    @Test
+    void givenIMESet_whenHaltWithInterruptPending_thenPCIncrementedNormally() {
+        int pc = 0xabcd;
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withMemory(IF_ADDRESS, 0x0f)
+                .withMemory(IE_ADDRESS, 0x0f)
+                .withInstructionRegister(0x01)
+                .withIME(true)
+                .withPC(pc)
+                .build();
+        Cpu cpu = new Cpu(cpuStructure);
+
+        cpu.cycle();
+
+        assertThatHex(cpuStructure.registers().PC()).isEqualTo((short) (SERIAL_HANDLER_ADDRESS + 1));
+    }
+
+    @Test
+    void givenIMEUnset_whenHaltBeforeInterruptPending_thenPCIncrementedNormally() throws TimeoutException {
+        int pc = 0xabcd;
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withMemory(IF_ADDRESS, 0x00)
+                .withMemory(IE_ADDRESS, 0x0f)
+                .withDecoder(opcode -> Halt.halt())
+                .withIME(false)
+                .withPC(pc)
+                .build();
+        Cpu cpu = new Cpu(cpuStructure);
+
+        Thread waitThread = new Thread(cpu::cycle);
+        Thread writeThread = new Thread(() -> cpuStructure.memory().write(IF_ADDRESS, (byte) 0x0f));
+
+        waitThread.start();
+        waitFor(() -> waitThread.getState() == Thread.State.WAITING || waitThread.getState() == Thread.State.BLOCKED);
+
+        writeThread.start();
+        waitFor(() -> !writeThread.isAlive());
+
+        waitFor(() -> !waitThread.isAlive());
+        assertThat(waitThread.isAlive()).isFalse();
+        Halt.halt().execute(cpuStructure);
+
+        assertThatHex(cpuStructure.registers().PC()).isEqualTo((short) (pc + 1));
+
+    }
+
+    @Test
+    void givenIMEUnset_whenHaltAndInterruptPending_thenPCFailsToIncrement() {
+        int pc = 0xabcd;
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withMemory(IF_ADDRESS, 0x0f)
+                .withMemory(IE_ADDRESS, 0x0f)
+                .withDecoder(opcode -> Halt.halt())
+                .withIME(false)
+                .withPC(pc)
+                .build();
+        Cpu cpu = new Cpu(cpuStructure);
+
+        cpu.cycle();
+
+        assertThatHex(cpuStructure.registers().PC()).isEqualTo((short) (pc));
     }
 
     private static class TestClock implements Clock {
