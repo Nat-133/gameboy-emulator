1b42173614addd01f1bd2636117d3d2a94a2b697
Nat Manley <nat@manleyfamily.co.uk>
2025-02-18 17:25:02 +0000

    Add prefix toString method.

--- src/main/java/org/gameboy/instructions/Prefix.java
+++ src/main/java/org/gameboy/instructions/Prefix.java
@@ -20,6 +20,11 @@ public class Prefix implements Instruction{
     }
 
     @Override
+    public String toString() {
+        return representation();
+    }
+
+    @Override
     public boolean equals(Object obj) {
         if (obj instanceof Instruction other) {
             return this.representation().equals(other.representation());
