8e8df38c646b41ddda43512d3abcb3f63888b62a
Nat Manley <nat@manleyfamily.co.uk>
2025-02-19 07:20:17 +0000

    Unprefixed decoder returns UNIMPLEMENTED.

--- src/main/java/org/gameboy/cpu/components/PrefixedOpcodeTable.java
+++ src/main/java/org/gameboy/cpu/components/PrefixedOpcodeTable.java
@@ -1,16 +1,11 @@
 package org.gameboy.cpu.components;
 
 import org.gameboy.cpu.instructions.Instruction;
-import org.gameboy.cpu.instructions.Nop;
 
-import static org.gameboy.cpu.utils.BitUtilities.uint;
+import static org.gameboy.cpu.instructions.Unimplemented.UNIMPLEMENTED;
 
 public class PrefixedOpcodeTable implements OpcodeTable {
     public Instruction lookup(byte opcode) {
-        int x = uint(opcode) >> 6;
-        int y = (uint(opcode) >> 3) & 0b00000111;
-        int z = uint(opcode) & 0b00000111;
-
-        return Nop.nop();
+        return UNIMPLEMENTED;
     }
 }
