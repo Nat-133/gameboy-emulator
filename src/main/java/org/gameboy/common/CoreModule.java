665ddd045a89e94786d106ddb2d5f0820928d6ac
Nat Manley <nat@manleyfamily.co.uk>
2025-08-17 19:06:43 +0100

    Add the starts of window fetching.

--- src/main/java/org/gameboy/common/CoreModule.java
+++ src/main/java/org/gameboy/common/CoreModule.java
@@ -60,6 +60,6 @@ public class CoreModule extends AbstractModule {
     @Singleton
     @Tac
     ByteRegister provideTacRegister() {
-        return new IntBackedRegister(0b0000_0100);
+        return new IntBackedRegister(0xF8);
     }
 }
