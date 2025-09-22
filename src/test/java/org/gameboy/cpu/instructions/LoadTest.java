a3bcfb39d285a2398243da30aa8f4452ba4a2950
Nat Manley <nat@manleyfamily.co.uk>
2025-09-22 16:58:52 +0100

    Add load tests for ld SP HL and ld HL sp+n

--- src/test/java/org/gameboy/cpu/instructions/LoadTest.java
+++ src/test/java/org/gameboy/cpu/instructions/LoadTest.java
@@ -1,9 +1,11 @@
 package org.gameboy.cpu.instructions;
 
 import org.gameboy.CpuStructureBuilder;
+import org.gameboy.cpu.Flag;
 import org.gameboy.cpu.components.CpuStructure;
 import org.gameboy.cpu.instructions.common.OperationTargetAccessor;
 import org.gameboy.cpu.instructions.targets.ByteRegister;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -14,6 +16,7 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.gameboy.GameboyAssertions.assertThatHex;
 import static org.gameboy.utils.BitUtilities.lower_byte;
 import static org.gameboy.utils.BitUtilities.set_lower_byte;
 
@@ -89,5 +92,74 @@ class LoadTest {
         byte memoryValueAfter = cpuStructure.memory().read(set_lower_byte((short) 0xff00, memoryLocation));
 
         assertThat(memoryValueAfter).isEqualTo(value);
+    }
+
+    @Test
+    void testLdHlSpPlusPositiveOffset() {
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withMemory(0x0000, 0x01)  // n = 1 (immediate value to be read)
+                .withSP(0xFFFE)
+                .withPC(0x0000)
+                .build();
+
+        Load.ld_HL_SP_OFFSET().execute(cpuStructure);
+
+        assertThatHex(cpuStructure.registers().HL()).isEqualTo((short) 0xFFFF);
+
+        assertThat(cpuStructure.registers().getFlag(Flag.Z)).isFalse();
+        assertThat(cpuStructure.registers().getFlag(Flag.N)).isFalse();
+        assertThat(cpuStructure.registers().getFlag(Flag.H)).isFalse();
+        assertThat(cpuStructure.registers().getFlag(Flag.C)).isFalse();
+
+        assertThatHex(cpuStructure.registers().SP()).isEqualTo((short) 0xFFFE);
+    }
+
+    @Test
+    void testLdHlSpPlusNegativeOffset() {
+        // LD HL,SP+n with negative offset (-1)
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withMemory(0x0000, 0xFF)  // n = -1
+                .withSP(0xFFFE)
+                .withPC(0x0000)
+                .build();
+
+        Load.ld_HL_SP_OFFSET().execute(cpuStructure);
+
+        assertThatHex(cpuStructure.registers().HL()).isEqualTo((short) 0xFFFD);
+
+        assertThat(cpuStructure.registers().getFlag(Flag.Z)).isFalse();
+        assertThat(cpuStructure.registers().getFlag(Flag.N)).isFalse();
+
+        assertThatHex(cpuStructure.registers().SP()).isEqualTo((short) 0xFFFE);
+    }
+
+    @Test
+    void testLdHlSpWithCarry() {
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withMemory(0x0000, 0x02)  // n = 2
+                .withSP(0x00FF)
+                .withPC(0x0000)
+                .build();
+
+        Load.ld_HL_SP_OFFSET().execute(cpuStructure);
+
+        assertThatHex(cpuStructure.registers().HL()).isEqualTo((short) 0x0101);
+
+        assertThat(cpuStructure.registers().getFlag(Flag.Z)).isFalse();
+        assertThat(cpuStructure.registers().getFlag(Flag.N)).isFalse();
+        assertThat(cpuStructure.registers().getFlag(Flag.C)).isTrue();
+    }
+
+    @Test
+    void testLdSpHl() {
+        CpuStructure cpuStructure = new CpuStructureBuilder()
+                .withHL(0x1234)
+                .withSP(0xFFFE)
+                .build();
+
+        Load.load_SP_HL().execute(cpuStructure);
+
+        assertThatHex(cpuStructure.registers().SP()).isEqualTo((short) 0x1234);
+        assertThatHex(cpuStructure.registers().HL()).isEqualTo((short) 0x1234);
     }
 }
