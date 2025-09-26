ed2f4a9478fe2d382877c74f4e32dabae1293864
Nat Manley <nat@manleyfamily.co.uk>
2025-09-26 08:33:31 +0100

    Replace fifo.pop() with fifo.pollFirst().

--- src/main/java/org/gameboy/display/PixelFifo.java
+++ src/main/java/org/gameboy/display/PixelFifo.java
@@ -34,11 +34,7 @@ public class PixelFifo {
     public Optional<TwoBitValue> read() {
         Optional<TwoBitValue> value;
         synchronized (fifo) {
-            try {
-                value = Optional.of(fifo.pop());
-            } catch (NoSuchElementException ignored) {
-                value = Optional.empty();
-            }
+            value = Optional.ofNullable(fifo.pollFirst());
         }
         readListeners.forEach(FifoReadListener::onRead);
         return value;
