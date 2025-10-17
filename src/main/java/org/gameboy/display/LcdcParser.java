fecb9a7db4be3bc9c3d61c5ba22d7c68d1cf9d08
Nat Manley <nat@manleyfamily.co.uk>
2025-10-17 16:17:15 +0100

    Make static methods in LcdcParser public.

--- src/main/java/org/gameboy/display/LcdcParser.java
+++ src/main/java/org/gameboy/display/LcdcParser.java
@@ -5,32 +5,32 @@ import static org.gameboy.common.MemoryMapConstants.TILE_MAP_B_ADDRESS;
 import static org.gameboy.utils.BitUtilities.get_bit;
 
 public class LcdcParser {
-    static boolean lcdEnabled(byte lcdc) {
+    public static boolean lcdEnabled(byte lcdc) {
         return get_bit(lcdc, 7);
     }
 
-    static int windowTileMap(byte lcdc) {
+    public static int windowTileMap(byte lcdc) {
         return get_bit(lcdc, 6) ? TILE_MAP_B_ADDRESS : TILE_MAP_A_ADDRESS;
     }
 
-    static boolean windowDisplayEnabled(byte lcdc) {
+    public static boolean windowDisplayEnabled(byte lcdc) {
         return get_bit(lcdc, 5);
     }
 
-    static boolean useUnsignedTileDataSelect(byte lcdc) {
+    public static boolean useUnsignedTileDataSelect(byte lcdc) {
         // if true, uses 8000 method, else uses 8800 method
         return get_bit(lcdc, 4);
     }
 
-    static int backgroundTileMap(byte lcdc) {
+    public static int backgroundTileMap(byte lcdc) {
         return get_bit(lcdc, 3) ? TILE_MAP_B_ADDRESS : TILE_MAP_A_ADDRESS;
     }
 
-    static int spriteSize(byte lcdc) {
+    public static int spriteSize(byte lcdc) {
         return get_bit(lcdc, 2) ? 16 : 8;
     }
 
-    static boolean backgroundAndWindowEnable(byte lcdc) {
+    public static boolean backgroundAndWindowEnable(byte lcdc) {
         return get_bit(lcdc, 0);
     }
 }
