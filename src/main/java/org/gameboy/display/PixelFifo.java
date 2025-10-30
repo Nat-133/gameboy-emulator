ed566965d4bcc7dfa54ca98d3e710638f29955a4
Nat Manley <nat@manleyfamily.co.uk>
2025-10-30 17:15:09 +0000

    Remove synchronisation from PixelFifo

--- src/main/java/org/gameboy/display/PixelFifo.java
+++ src/main/java/org/gameboy/display/PixelFifo.java
@@ -8,42 +8,22 @@ import java.util.*;
 public class PixelFifo {
     private final Deque<TwoBitValue> fifo = new ArrayDeque<>(8);
     private final List<FifoReadListener> readListeners = new ArrayList<>();
-    // fifo will have 8 pixel queue,
-    // + 1 pixel (next pixel value) that will be pulled from the queue.
-    // ensures that there will always be a pixel to be pushed to the display
-
-    // still need to find a better way, as this could still cause a 1 cycle delay on the Background fetcher
-
-    // another solution might be to make the queue observable,
-    // and add a listener that the background fetcher can register
-
-    // another might be to ensure fifo fill happens last
-    // maybe listener on pixel pop? But how to guarantee lister is added before pop
-    // maybe listener is long living. Added on fifo creation, and has `pop occurred`
-    // we can wait for `pop occurred`
-    // javafx.beans ObservableBooleanValue
 
     public void write(List<TwoBitValue> values) {
-        synchronized (fifo) {
-            values.stream()
-                    .skip(fifo.size())
-                    .forEach(fifo::add);
-        }
+        values.stream()
+                .skip(fifo.size())
+                .forEach(fifo::add);
     }
 
     public Optional<TwoBitValue> read() {
         Optional<TwoBitValue> value;
-        synchronized (fifo) {
-            value = Optional.ofNullable(fifo.pollFirst());
-        }
+        value = Optional.ofNullable(fifo.pollFirst());
         readListeners.forEach(FifoReadListener::onRead);
         return value;
     }
 
     public boolean isEmpty() {
-        synchronized (fifo) {
-            return fifo.isEmpty();
-        }
+        return fifo.isEmpty();
     }
 
     public int size() {
@@ -55,8 +35,6 @@ public class PixelFifo {
     }
 
     public void clear() {
-        synchronized (fifo) {
-            fifo.clear();
-        }
+        fifo.clear();
     }
 }
