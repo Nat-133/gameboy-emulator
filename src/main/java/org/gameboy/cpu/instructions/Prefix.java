244ab5a3cbf858da721f707cedc874b3ac355c76
Nat Manley <nat@manleyfamily.co.uk>
2026-02-26 10:22:29 +0000

    fix: prevent interrupt between CB prefix and operand

--- src/main/java/org/gameboy/cpu/instructions/Prefix.java
+++ src/main/java/org/gameboy/cpu/instructions/Prefix.java
@@ -1,6 +1,7 @@
 package org.gameboy.cpu.instructions;
 
 import org.gameboy.cpu.components.CpuStructure;
+import org.gameboy.cpu.instructions.common.ControlFlow;
 
 public class Prefix implements Instruction{
     private Prefix() {}
@@ -12,6 +13,17 @@ public class Prefix implements Instruction{
     @Override
     public void execute(CpuStructure cpuStructure) {
         cpuStructure.decoder().switchTables();
+
+        byte ir = cpuStructure.memory().read(cpuStructure.registers().PC());
+        cpuStructure.registers().setInstructionRegister(ir);
+        ControlFlow.incrementPC(cpuStructure);
+
+        cpuStructure.clock().tick();
+    }
+
+    @Override
+    public boolean handlesFetch() {
+        return true;
     }
 
     @Override
